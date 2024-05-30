dfb8505be570de6af2f0f9c369b038047e6ec197
tai <taisinclair4@gmail.com>
2024-05-30 20:25:10 +0100

    adds prefer git to treesitter

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,3 +1,5 @@
+require("nvim-treesitter.install").prefer_git = true
+
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
   ensure_installed = { "javascript", "python", "c", "lua", "vim", "vimdoc", "query" },
