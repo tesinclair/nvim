34f95c4097aea9604e2cac480ae49ef99ed76d2a
Tai Sinclair <taisinclair4@gmail.com>
2024-06-03 08:50:45 +0100

    adds new remaps

--- lua/myfuckingshit/remap.lua
+++ lua/myfuckingshit/remap.lua
@@ -1,5 +1,7 @@
 vim.g.mapleader = ' '
-vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
+
+vim.keymap.set('n', '<leader>jf', vim.cmd.Ex)
+
 -- harpoon
 vim.keymap.set('n', '<leader>hm', function()
     require("harpoon.ui").toggle_quick_menu()
@@ -26,25 +28,9 @@ vim.keymap.set('n', 'yy', '0V$"+y<Esc>');
 
 vim.keymap.set('n', '<leader>ut', vim.cmd.UndotreeToggle)
 
--- Fugitive
-
-vim.keymap.set('n', '<leader>gs', vim.cmd.Git)
-
-
--- Std remaps
--- Heavily *borrowed* from the primeagen
-
-vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
-vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
-
-vim.keymap.set('n', 'J', 'mzJ`z')
-vim.keymap.set('n', '<C-d>', '<C-d>zz')
-vim.keymap.set('n', '<C-u>', '<C-u>zz')
 vim.keymap.set('n', 'n', 'nzzzv')
-vim.keymap.set('n', 'N', 'Nzzzv')
 
 vim.keymap.set('n', 'Q', '<nop>')
-vim.keymap.set('n', '<C-f>', '<cmd>silent !tmux neww tmux-sessionizer<CR>')
 
 -- My Keymaps
 vim.keymap.set('n', '<leader>df', 'VGd')
@@ -55,21 +41,25 @@ vim.keymap.set('n', '<leader>fa', 'ggVG=<C-o><Esc>')
 -- Make quick code runner
 vim.keymap.set('n', '<leader>r', ':lua run_code()<CR>', {noremap = true})
 
+-- Delete all
 vim.keymap.set('n', '<leader>da', ':%d<CR>')
 
+-- Copy all into "+
 vim.keymap.set('n', '<leader>ca', 'ggVG"+y<Esc>')
 
 -- Making :w and :wq easier
 vim.keymap.set('n', '<leader>w', ':w<CR>')
-vim.keymap.set('n', '<leader>q', ':wq<CR>')
+vim.keymap.set('n', '<leader>wq', ':wq<CR>')
+vim.keymap.set('n', '<leader>q', ':q<CR>')
 
 -- u as undo really messes me up (it is way too close to p and i)
 vim.keymap.set('n', 'u', '<nop>')
 vim.keymap.set('n', '<leader>uu', ':u<CR>')
 
 -- exit to current dir
-vim.keymap.set('n', '<leader>qc', ':lua exit_current_dir()<CR>')
+vim.keymap.set('n', '<leader>cd', ':lua exit_current_dir()<CR>')
 
--- Change normal mode to n, cos esc is pretty far
-vim.keymap.set('v', 'jkl;', '<Esc>')
-vim.keymap.set('i', 'jkl;', '<Esc>')
+-- Change normal mode to hjkl because your hand is already there
+-- if you want to start normal mode
+vim.keymap.set('v', 'hjkl', '<Esc>')
+vim.keymap.set('i', 'hjkl', '<Esc>')
